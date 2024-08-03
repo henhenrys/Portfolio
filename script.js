8002648a26b3f46a8e33d9e82eb57a94441e4679
henhenrys <tranhenry94@gmail.com>
2024-08-03 04:08:33 -0700

    missing projects

---
+++ script.js
@@ -0,0 +1,34 @@
+var num = 0;
+
+function scrollProjects() {
+    document.getElementById("projects").scrollIntoView();
+}
+
+function darkMode() {
+    if(num % 2 == 0) {
+        document.body.style.backgroundColor = "#121212";
+        document.querySelector("h1").style.color = "white";
+        document.querySelector("h2").style.color = "white";
+        document.querySelector("h5").style.color = "white";
+        document.getElementById("changeToo").style.color = "white";
+        document.getElementById("changeToo2").style.color = "white";
+        document.getElementById("changeToo3").style.color = "white";
+        document.querySelector("i").classList.toggle("fa-sun");
+        document.querySelector("i").classList.toggle("fa-moon");
+        document.querySelector("i").style.color = "yellow";
+        num++;
+    }
+    else {
+        document.body.style.backgroundColor = "#fffff1";
+        document.querySelector("h1").style.color = "black";
+        document.querySelector("h2").style.color = "black";
+        document.querySelector("h5").style.color = "black";
+        document.getElementById("changeToo").style.color = "black";
+        document.getElementById("changeToo2").style.color = "black";
+        document.getElementById("changeToo3").style.color = "black";
+        document.querySelector("i").classList.toggle("fa-sun");
+        document.querySelector("i").classList.toggle("fa-moon");
+        document.querySelector("i").style.color = "black";
+        num++;
+    }
+}
